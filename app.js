76a2227f949b11306bf4b236be7584311a5dba68
Danijel <you@example.com>
2019-05-22 10:15:43 +0200

    dok ne nadjem svoje slike

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var app = express();
+const path = require('path');
+const express = require('express');
 
+
+var app = express();
 const port = process.env.PORT || 3000;
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+
+// Setup handlebars engine and views location
 app.set("view engine", "ejs");
+
+
+// Setup static directory to serve
+//app.use(express.static(publicDirectoryPath));
+app.use( express.static( "public" ));
 
 app.get("/", function(req, res){
     res.render("home");
@@ -12,5 +24,5 @@ app.get("/", function(req, res){
 
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
+    console.log('Server is up on port ' + port);
 });
